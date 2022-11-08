af3375e39f0e9da8345441c4b601b331e307a634
raulpressel <raulpressel@gmail.com>
2022-11-08 15:49:14 -0300

    fix get img by cat

--- db/food/getImgByCategoryId.go
+++ db/food/getImgByCategoryId.go
@@ -14,8 +14,8 @@ func GetImgByCategoryId(id int) ([]dtos.ImgFoodByCategoryResponse, error) {
 	err := db.Table("food_categories").
 		Select("foods.title, location_imgs.location").
 		Joins("left JOIN foods ON food_categories.food_id = foods.id").
-		Joins("left JOIN location_imgs ON categories.location_id = location_imgs.id").
-		Where("categories.active = 1 and categories.id = ?", id).
+		Joins("left JOIN location_imgs ON foods.location_id = location_imgs.id").
+		Where("food_categories.id = ?", id).
 		Scan(&responseModel).Error
 
 	return responseModel, err
