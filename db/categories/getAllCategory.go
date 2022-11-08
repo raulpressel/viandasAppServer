ee0558be19586a9495ac62d00b2d8d8094adf86e
raulpressel <raulpressel@gmail.com>
2022-11-08 16:12:10 -0300

    fix category color

--- db/categories/getAllCategory.go
+++ db/categories/getAllCategory.go
@@ -12,7 +12,7 @@ func GetAllCategory() ([]dtos.AllCategoryResponse, error) {
 	var responseModel []dtos.AllCategoryResponse
 
 	err := db.Table("categories").
-		Select("categories.id, categories.description, categories.title, categories.price, location_imgs.location").
+		Select("categories.id, categories.description, categories.title, categories.color, categories.price, location_imgs.location").
 		Joins("left JOIN location_imgs ON categories.location_id = location_imgs.id").
 		Where("categories.active = 1").
 		Scan(&responseModel).Error
