a4712cbe9cbe8bbd5ecaca4a33828da46728382d
raulpressel <raulpressel@gmail.com>
2022-12-30 15:53:55 -0300

    correccion consulta

--- db/client/getAllClientByTandas.go
+++ db/client/getAllClientByTandas.go
@@ -37,7 +37,7 @@ func GetAllClientByTandas(tandas []int) (*[]dtos.Client, error) {
 	var modelClient []models.Client
 
 	err := db.Table("clients").
-		Select("clients.id, clients.name, clients.last_name, clients.email, clients.id_user_kl, clients.phone_primary, clients.phone_secondary, clients.observation, clients.born_date").
+		Select("distinct clients.id, clients.name, clients.last_name, clients.email, clients.id_user_kl, clients.phone_primary, clients.phone_secondary, clients.observation, clients.born_date").
 		Joins("JOIN client_addresses ON client_addresses.client_id = clients.id").
 		Where("client_addresses.address_id IN (select address_id from tanda_addresses where tanda_id IN (?))", tandas).
 		Scan(&modelClient).Error
